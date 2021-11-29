196aea0b6bf78c79a2ea2bd0b01aa8bc8aac7bad
Mateus Sabino <mateussabino87@gmail.com>
2021-11-29 12:48:22 -0300

    feat: Adiciona Coop::Biddings::Lots::LotQuestionsController

---
+++ app/controllers/coop/biddings/lots/lot_questions_controller.rb
@@ -0,0 +1,46 @@
+module Coop
+  class Biddings::Lots::LotQuestionsController < CoopController
+    include CrudController
+
+    load_and_authorize_resource except: [:update]
+
+    before_action :set_lot_answer_user_id, only: :update
+
+    PERMITTED_PARAMS = [
+      :id, lot_answer_attributes: [
+        :id, :answer
+      ]
+    ].freeze
+
+    expose :bidding
+    expose :lot
+    expose :lot_question
+    expose :lot_questions, -> { find_lot_questions }
+
+    private
+
+    def resource
+      lot_question
+    end
+
+    def resources
+      lot_questions
+    end
+
+    def default_sort_scope
+      resources
+    end
+
+    def find_lot_questions
+      LotQuestion.accessible_by(current_ability).by_lot(lot)
+    end
+
+    def lot_question_params
+      params.require(:lot_question).permit(*PERMITTED_PARAMS)
+    end
+
+    def set_lot_answer_user_id
+      resource.build_lot_answer.user = current_user
+    end
+  end
+end
