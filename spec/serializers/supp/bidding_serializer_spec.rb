d3488f491100351af416dff64c8044b7412ec9f2
Mateus Sabino <mateussabino87@gmail.com>
2021-12-07 12:52:43 -0300

    feat: Adiciona campo show_new_button_question no Supp::BiddingSerializer

--- spec/serializers/supp/bidding_serializer_spec.rb
+++ spec/serializers/supp/bidding_serializer_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Supp::BiddingSerializer, type: :serializer do
   it_behaves_like 'a bidding_serializer'
+
+  describe 'extra attributes' do
+    describe 'show_new_button_question' do
+      subject { format_json(described_class, object) }
+  
+      let(:closing_date) { Date.today }
+      let(:object) { create :bidding, closing_date: closing_date }
+  
+      context 'when close to closing_date' do
+        it { is_expected.to include 'show_new_button_question' => false }
+      end
+  
+      context 'when not close to closing_date' do
+        let(:closing_date) { Date.today + 2.days}
+
+        it { is_expected.to include 'show_new_button_question' => true }
+      end
+    end
+  end
+
+
 end
