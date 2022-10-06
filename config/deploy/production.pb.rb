657dbf82c8e22c0a2a3796da2da702f3d99fb549
Mateus Sabino <mateussabino87@gmail.com>
2022-10-06 09:40:03 -0300

    feat: Atualiza ssh_options do deploy da PB

--- config/deploy/production.pb.rb
+++ config/deploy/production.pb.rb
@@ -1,7 +1,8 @@
-server "138.185.35.11", user: "sol", roles: %w{app db web sidekiq} # api.sol.car.ba.gov.br
+server "138.185.35.11", user: "sol", roles: %w{app db web sidekiq} # api.sol.cooperar.pb.gov.br
 set :sidekiq_service, 'production.sol-api.sidekiq.service'
 set :application, "sol-api"
 set :stage, 'production'
 set :cron_user, 'sol'
+set :ssh_options, { port: 22011 }
 
 set :repo_url, "https://github.com/caiena/sol-api.git"
