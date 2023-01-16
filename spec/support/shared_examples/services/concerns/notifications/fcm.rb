87a10ff1cd32d3e8b0c396f7672bb87ea35ea396
Mateus Sabino <mateussabino87@gmail.com>
2023-01-16 16:50:36 -0300

    Ajustes no FCM (#4)
    * fix: Adiciona o status "cancelado" de um lote nas condições de encerramento de uma licitação
    
    * fix: Adiciona validação do status "cancelado" de um lote ao encerrar novamente uma licitação
    
    * feat: Atualiza endpoint Administrator::Biddings::RegenerateMinutesController para considerar as situações Fracassada, Deserta e Concluída
    
    * fix: Retorna mecanismo de fracassar licitação no admin
    
    * feat: Ajusta importador de fornecedores
    
    * feat: Remove envio de push notification
    
    Co-authored-by: MarivalViana <marivalvmf@gmail.com>
    Co-authored-by: alexandrecrvg <alexandrecrvg@gmail.com>

--- spec/support/shared_examples/services/concerns/notifications/fcm.rb
+++ spec/support/shared_examples/services/concerns/notifications/fcm.rb
@@ -4,10 +4,16 @@ RSpec.shared_examples 'services/concerns/notifications/fcm' do |times|
   describe 'fcm' do
     let(:notification) { create(:notification) }
     let(:fcm_response) { double('call', call: true) }
+    let(:send_fcm_notification) { true }
 
     before do
       allow(::Notifications::Fcm).to receive(:delay).and_return(fcm_response)
       allow(Notification).to receive(:create).and_return(notification_response)
+      # XXX: A configuração do envio de push notification para a PB não é possível no momento,
+      # pois a atualização da gem fcm entra em conflito com outras gems que utilizam
+      # versões diferentes da gem Faraday.
+      # Assim que o sistema puder ser atualizado e testado, a funcionalidade poderá ser validada novamente.
+      allow(service).to receive(:send_fcm_notification).and_return(send_fcm_notification)
 
       service.call
     end
@@ -20,6 +26,13 @@ RSpec.shared_examples 'services/concerns/notifications/fcm' do |times|
 
     context 'when not created notification' do
       let(:notification_response) { nil }
+
+      it { expect(::Notifications::Fcm).not_to have_received(:delay) }
+    end
+
+    context 'when send_fcm_notification param is false' do
+      let(:send_fcm_notification) { false }
+      let(:notification_response) { notification }
 
       it { expect(::Notifications::Fcm).not_to have_received(:delay) }
     end
